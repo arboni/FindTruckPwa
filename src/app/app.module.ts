facda7ea7edc3486b156d50f49c0d36c023ff837
Loir Arbao Junior <arboniguitar@gmail.com>
2019-09-29 19:19:48 -0300

    upgrading routes
    upgrading routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,29 @@ import { AppComponent } from './app.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { AgmCoreModule} from '@agm/core';
+import { AgmCoreModule } from '@agm/core';
+import { HttpClientModule} from '@angular/common/http';
+import { GeoComponent } from './geo/geo.component';
+import { RouterModule } from '@angular/router';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroComponent
+    HttpClientModule,
+    CadastroComponent,
+    GeoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AgmCoreModule.forRoot({apiKey: 'AIzaSyAFWuTsunP3aVWHD1h-OGH48L_T3OdcwMI'}),
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    RouterModule.forRoot([
+      { path: '', component: GeoComponent },
+      { path: 'app-geo/:GeoComponent', component: GeoComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/geo/geo.component.ts
+++ src/app/geo/geo.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ApplicationInitStatus } from '@angular/core';
+import { mapToMapExpression } from '@angular/compiler/src/render3/util';
+
 
 @Component({
   selector: 'app-geo',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./geo.component.css']
 })
 export class GeoComponent implements OnInit {
-
+  texto : string = 'Empresa - Senac RS';
+  lat: number = -30.03530948;
+  lng: number = -51.22653902;
+  zoom: number = 15;
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
